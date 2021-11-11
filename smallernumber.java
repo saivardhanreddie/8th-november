f77725e578fea1fbe905a2e76cef1781da87c671
saivardhanreddie <93024740+saivardhanreddie@users.noreply.github.com>
2021-11-11 10:29:20 +0530

    Add files via upload

---
+++ smallernumber.java
@@ -0,0 +1,17 @@
+class smallernumber {
+
+    public static void main(String[] args) {
+        int a = 30;
+        int b = 25;
+
+        if (a < b) {
+            System.out.println("a smaller than b");
+        } else if (b < a) {
+            System.out.println("b smaller than a");
+        } else {
+            System.out.println("a equal to b");
+        }
+
+    }
+
+}
